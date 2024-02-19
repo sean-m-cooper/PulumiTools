50706213cfc9fd140dd35b818a3181b8ff024acf
Sean Cooper <sean.m.cooper.ak@gmail.com>
2024-02-19 15:27:19 -0800

    update files to remove secrets

--- PulumiDemo/Assets/mountDrive.ps1
+++
@@ -1,9 +0,0 @@
-$connectTestResult = Test-NetConnection -ComputerName sascdev1540992ded.file.core.windows.net -Port 445
-if ($connectTestResult.TcpTestSucceeded) {
-    # Save the password so the drive will persist on reboot
-    cmd.exe /C "cmdkey /add:`"sascdev1540992ded.file.core.windows.net`" /user:`"localhost\sascdev1540992ded`" /pass:`"R3b+pgXRCeUYtxFZDUstPFIQAOM6voYiscerBZIwsgxRKvtOUpIUn5M4l4sX8Jhr2vuoz0heXqrs4P2uFRLtkQ==`""
-    # Mount the drive
-    New-PSDrive -Name Z -PSProvider FileSystem -Root "\\sascdev1540992ded.file.core.windows.net\fsbotshare" -Persist
-} else {
-    Write-Error -Message "Unable to reach the Azure storage account via port 445. Check to make sure your organization or ISP is not blocking port 445, or use Azure P2S VPN, Azure S2S VPN, or Express Route to tunnel SMB traffic over a different port."
-}
